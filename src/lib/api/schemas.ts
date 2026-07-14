2a67c77eab718988f16c8b49654e622bca4212bf
Dylan Fraser <TOO_LAZY_TO_PUT_AN_EMAIL>
2026-07-14 01:03:43 -0400

    Refactor schema URL definitions to string unions

--- src/lib/api/schemas.ts
+++ src/lib/api/schemas.ts
@@ -1,20 +1,13 @@
-export enum SCHEMA {
-  Search,
-  QuickSearch,
-  Page,
-  Media,
-}
+export type Schema = "search" | "quickSearch" | "page" | "media";
 
-export const SCHEMA_KEY = {
-  [SCHEMA.Search]: `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={query}&srnamespace=0&format=json&origin=*`,
-  [SCHEMA.QuickSearch]: `https://en.wikipedia.org/w/rest.php/v1/search/title?q={query}&limit=5`,
-  [SCHEMA.Page]: `https://en.wikipedia.org/w/api.php?action=parse&page={query}&prop=text&format=json&origin=*`,
-  [SCHEMA.Media]: `https://en.wikipedia.org/api/rest_v1/page/media-list/{query}`,
+const SCHEMA_URLS: Record<Schema, string> = {
+  search:
+    "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={query}&srnamespace=0&format=json&origin=*",
+  quickSearch: "https://en.wikipedia.org/w/rest.php/v1/search/title?q={query}&limit=5",
+  page: "https://en.wikipedia.org/w/api.php?action=parse&page={query}&prop=text&format=json&origin=*",
+  media: "https://en.wikipedia.org/api/rest_v1/page/media-list/{query}",
 };
 
-export function encodeSchemaURL(id: SCHEMA, query: string) {
-  const base_url = SCHEMA_KEY[id];
-  const url = base_url.replace("{query}", encodeURIComponent(query));
-
-  return url;
+export function encodeSchemaURL(id: Schema, query: string): string {
+  return SCHEMA_URLS[id].replace("{query}", encodeURIComponent(query));
 }
